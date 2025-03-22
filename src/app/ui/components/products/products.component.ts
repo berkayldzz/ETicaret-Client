a64a2296bbfc3c1c4e05219c3c18c314b8c9ba10
Berkay Yıldız <yildizberkay359@gmail.com>
2025-03-22 14:08:35 +0300

    register page added

--- src/app/ui/components/products/products.component.ts
+++ src/app/ui/components/products/products.component.ts
@@ -12,6 +12,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
     super(spinner);
   }
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
+   // this.showSpinner(SpinnerType.BallAtom);
   }
 }
