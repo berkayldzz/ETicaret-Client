ea0ab37a0be73da3767d1481f067dba187d82a8d
Berkay Yıldız <yildizberkay359@gmail.com>
2025-05-15 20:26:32 +0300

    Dynamic Component Loading Altyapısının Kurulması

--- src/app/directives/common/dynamic-load-component.directive.ts
+++ src/app/directives/common/dynamic-load-component.directive.ts
@@ -1,10 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appDynamicLoadComponent]'
 })
 export class DynamicLoadComponentDirective {
 
-  constructor() { }
+  constructor(public viewContainerRef: ViewContainerRef) { }
 
 }
